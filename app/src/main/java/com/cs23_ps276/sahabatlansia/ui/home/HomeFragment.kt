7fbde600428a3131facb55683bb941ddb76071e4
Akumaaaaaa <akuma@students.undip.ac.id>
2023-06-10 18:01:03 +0700

    Update 10 Juni 2023

--- app/src/main/java/com/cs23_ps276/sahabatlansia/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cs23_ps276/sahabatlansia/ui/home/HomeFragment.kt
@@ -1,40 +1,57 @@
 package com.cs23_ps276.sahabatlansia.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.cs23_ps276.sahabatlansia.CariCaregiverActivity
+import com.cs23_ps276.sahabatlansia.R
 import com.cs23_ps276.sahabatlansia.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val homeViewModel =
-        ViewModelProvider(this)[HomeViewModel::class.java]
+    private lateinit var homeViewModel: HomeViewModel
 
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+
+        binding.appTitle.text = getString(R.string.app_name)
+        binding.appMotto.text = getString(R.string.app_motto)
+        binding.appVersion.text = getString(R.string.app_version_1_0)
+
+        // Set up click listeners for the buttons
+        binding.caregiverButton.setOnClickListener {
+            val intent = Intent(activity, CariCaregiverActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.articleButton.setOnClickListener {
+            //val intent = Intent(activity, ArticleActivity::class.java)
+            //startActivity(intent)
+        }
+
+        binding.notificationButton.setOnClickListener {
+            //val intent = Intent(activity, NotifikasiActivity::class.java)
+            //startActivity(intent)
+        }
+
+        return root
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
