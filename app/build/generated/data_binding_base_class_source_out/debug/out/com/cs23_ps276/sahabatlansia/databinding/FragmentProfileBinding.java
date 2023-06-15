1ea5f1614a986176f9f3298008a0c8ea4ad604e1
alvfo <alvaro28102001@gmail.com>
2023-06-15 21:15:49 +0700

    Update Detail Caregiver

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/cs23_ps276/sahabatlansia/databinding/FragmentProfileBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/cs23_ps276/sahabatlansia/databinding/FragmentProfileBinding.java
@@ -4,11 +4,13 @@ package com.cs23_ps276.sahabatlansia.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
 import com.cs23_ps276.sahabatlansia.R;
@@ -21,15 +23,24 @@ public final class FragmentProfileBinding implements ViewBinding {
   private final ConstraintLayout rootView;
 
   @NonNull
+  public final Button btnCariCaregiver;
+
+  @NonNull
   public final RelativeLayout relativeLayout;
+
+  @NonNull
+  public final RecyclerView rvCaregiver;
 
   @NonNull
   public final TextView textView6;
 
   private FragmentProfileBinding(@NonNull ConstraintLayout rootView,
-      @NonNull RelativeLayout relativeLayout, @NonNull TextView textView6) {
+      @NonNull Button btnCariCaregiver, @NonNull RelativeLayout relativeLayout,
+      @NonNull RecyclerView rvCaregiver, @NonNull TextView textView6) {
     this.rootView = rootView;
+    this.btnCariCaregiver = btnCariCaregiver;
     this.relativeLayout = relativeLayout;
+    this.rvCaregiver = rvCaregiver;
     this.textView6 = textView6;
   }
 
@@ -60,9 +71,21 @@ public final class FragmentProfileBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
+      id = R.id.btn_cari_caregiver;
+      Button btnCariCaregiver = ViewBindings.findChildViewById(rootView, id);
+      if (btnCariCaregiver == null) {
+        break missingId;
+      }
+
       id = R.id.relativeLayout;
       RelativeLayout relativeLayout = ViewBindings.findChildViewById(rootView, id);
       if (relativeLayout == null) {
+        break missingId;
+      }
+
+      id = R.id.rv_caregiver;
+      RecyclerView rvCaregiver = ViewBindings.findChildViewById(rootView, id);
+      if (rvCaregiver == null) {
         break missingId;
       }
 
@@ -72,7 +95,8 @@ public final class FragmentProfileBinding implements ViewBinding {
         break missingId;
       }
 
-      return new FragmentProfileBinding((ConstraintLayout) rootView, relativeLayout, textView6);
+      return new FragmentProfileBinding((ConstraintLayout) rootView, btnCariCaregiver,
+          relativeLayout, rvCaregiver, textView6);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
