e34a08240c78836bf372b2567af16b56a4a47883
Akumaaaaaa <akuma@students.undip.ac.id>
2023-06-16 14:15:31 +0700

    Update Final Project

--- app/src/main/java/com/cs23_ps276/sahabatlansia/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/cs23_ps276/sahabatlansia/ui/profile/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.cs23_ps276.sahabatlansia.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cs23_ps276.sahabatlansia.LansiaFormActivity
 import com.cs23_ps276.sahabatlansia.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -19,6 +21,11 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.btnAddLansia.setOnClickListener {
+            val intent = Intent(activity, LansiaFormActivity::class.java)
+            startActivity(intent)
+        }
 
         return root
     }
